3e8ebbf162f13ab83edad5e174d90491e4b82dc8
Gyanendra Prakash <prakash.gyanendra5@gmail.com>
2021-10-21 02:42:33 +0530

    added skeleton for authentication

---
+++ .next/static/webpack/webpack.ee2e0bcd4514049e15c0.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "4eb65cffd057f5bd145d"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay5lZTJlMGJjZDQ1MTQwNDllMTVjMC5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjRlYjY1Y2ZmZDA1N2Y1YmQxNDVkXCI7IH0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
